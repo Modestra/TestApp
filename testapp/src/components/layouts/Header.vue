b0ef4fcfd8b008ff77a875102b3683194f83fe7e
Modestra Mod <umbrellamixmail@gmail.com>
2024-03-18 23:49:17 +0700

    Добавление стилей

--- testapp/src/components/layouts/Header.vue
+++ testapp/src/components/layouts/Header.vue
@@ -1,9 +1,27 @@
 <template>
     <header>
-        <div class="container"></div>
+        <div class="container container-header">
+            <div class="navigation">
+                <div class="navigation-logo">
+                    <img src="\src\assets\images\Logo.svg" alt="" />
+                    <p class="main-text">Умко</p>
+                </div>
+            </div>
+        </div>
+        <section class="container-info">
+            <div class="header-block"></div>
+            <div class="header-block"></div>
+        </section>
     </header>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+header {
+    width: 100%;
+    height: 600px;
+    border-radius: 5px;
+    box-shadow: 0px 0px 8px 2px rgba(34, 60, 80, 0.2);
+}
+</style>
