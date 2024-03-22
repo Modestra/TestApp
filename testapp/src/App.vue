f9d92daf325388db59bd727884b49363adf68ec5
Modestra Mod <umbrellamixmail@gmail.com>
2024-03-22 12:52:37 +0700

    Подключение формы

--- testapp/src/App.vue
+++ testapp/src/App.vue
@@ -6,6 +6,14 @@
                 <label class="selector-label label-info">Название курса</label>
                 <input class="selector-input" />
             </div>
+            <div class="selector-container">
+                <label class="selector-label label-info">Стоимость</label>
+                <input class="selector-input" />
+            </div>
+            <div class="selector-container">
+                <label class="selector-label label-info">Тип курса</label>
+                <input class="selector-input" />
+            </div>
             <div class="selector-container"></div>
             <div class="selector-container"></div>
             <div class="selector-container"></div>
@@ -32,6 +40,18 @@
     </div>
     <div class="container">
         <section class="catalog-allcourse"></section>
+    </div>
+    <div class="container">
+        <div class="addblock"></div>
+    </div>
+    <div class="container question">
+        <div class="question-logo">
+            <p>Остались вопросы? Напишите нам и мы ответим вам на все!</p>
+            <img src="\src\assets\images\MiddleLogo.svg" alt="Изображение" />
+        </div>
+        <div class="question-block">
+            <div class="question-form"></div>
+        </div>
     </div>
     <Footer></Footer>
 </template>
@@ -61,6 +81,77 @@ const cards = ref([{}]);
         height: 48px;
         margin-top: 50px;
         margin-bottom: 50px;
+    }
+}
+.addblock {
+    width: 80%;
+    height: 600px;
+    background: linear-gradient(
+        90deg,
+        rgb(255, 240, 106),
+        rgb(246, 190, 67) 65%
+    );
+    border-radius: 25px;
+    margin-bottom: 50px;
+}
+.question {
+    &-block {
+        display: flex;
+        justify-content: end;
+        width: 40%;
+        height: 100%;
+    }
+    &-logo {
+        display: flex;
+        flex-direction: column;
+        width: 40%;
+        height: 662px;
+    }
+    &-logo img {
+        width: 431px;
+        height: 463px;
+        margin-left: 15px;
+    }
+    &-logo p {
+        width: 80%;
+        font-family: Plovdiv Display;
+        font-size: 42px;
+        font-weight: 400;
+        line-height: 50.4px;
+        text-align: left;
+    }
+    &-form {
+        width: 592px;
+        height: 543px;
+        background-color: #f3f3f3;
+    }
+}
+@media (max-width: 1024px) {
+    .question {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 100%;
+        &-logo {
+            width: 80%;
+            height: 100%;
+            margin-bottom: 5px;
+        }
+        &-logo p {
+            width: 100%;
+            text-align: center;
+        }
+        &-logo img {
+            display: none;
+        }
+        &-block {
+            width: 80%;
+            height: 100%;
+            margin-bottom: 15px;
+        }
+        &-form {
+            width: 100%;
+        }
     }
 }
 </style>
--- testapp/src/components/layouts/Footer.vue
+++ testapp/src/components/layouts/Footer.vue
@@ -8,5 +8,6 @@
 footer {
     width: 100%;
     height: 216px;
+    background-color: #787878;
 }
 </style>
